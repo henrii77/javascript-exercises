2f628792fb7471b815243de9b57ce7d148b46956
henrii77 <henriiw480@gmail.com>
2025-01-22 14:06:59 +1100

    finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+    let original = sentence.toLowerCase().split("").filter(char => {
+        return /[a-zA-Z0-9]/.test(char);
+    }).join("");
+    let reversed = sentence.toLowerCase().split("").reverse().filter(char => {
+        return /[a-zA-Z0-9]/.test(char);
+    }).join("");
+    return original === reversed
 };
 
 // Do not edit below this line
