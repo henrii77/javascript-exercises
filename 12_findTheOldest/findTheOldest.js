0b302ed66bf5a8151372cf639120eabd9dcfc06e
henrii77 <henriiw480@gmail.com>
2025-01-22 15:14:45 +1100

    found the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    sortedByAge = people.sort((a, b) => {
+        const aAge = 'yearOfDeath' in a ? 
+            a.yearOfDeath - a.yearOfBirth :
+            (new Date()).getFullYear - a.yearOfBirth;
+        
+        const bAge = 'yearOfDeath' in b ? 
+            b.yearOfDeath - b.yearOfBirth :
+            (new Date()).getFullYear - b.yearOfBirth;
+        
+        return aAge > bAge ? 1 : -1;
+    })
+    return sortedByAge.at(-1);
 };
 
 // Do not edit below this line
